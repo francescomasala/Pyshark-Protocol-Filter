cc360ff470a002032d7084eb83c2a71f2b36d7c8
Francesco Masala <mail@francescomasala.me>
2021-12-07 21:46:15 +0100

    Now the "lib" can output a json file
    In the future I will release a wiki page for every single package, maybe I'll host the wiki to gitlab, who knows

--- Lib/Cisco.py
+++ Lib/Cisco.py
@@ -1,64 +1,96 @@
+import json
+
+
 class HSRP :
-    def __init__(self, packet) :
+    def __init__(self, packet):
         self.HsrpPacket = packet.hsrp
         self.packet = packet
 
-    def GetSourceRouter(self) :
+    def GetHighestLayerName(self):
+        return self.packet.highest_layer
+
+    def GetSourceRouter(self):
         return str(self.packet.ip.src)
 
-    def GetDestRouter(self) :
+    def GetDestRouter(self):
         return str(self.packet.ip.dst)
 
-    def GetRouterMode(self) :
+    def GetRouterMode(self):
         routerMode = int(self.HsrpPacket.opcode)
-        if routerMode == 0 :
+        if routerMode == 0:
             return str("Hello")
-        elif routerMode == 3 :
+        elif routerMode == 3:
             return str("Advertise")
 
-    def GetRouterState(self) :
+    def GetRouterState(self):
         routerState = int(self.HsrpPacket.state)
-        if routerState == 16 :
+        if routerState == 16:
             return str("Active")
-        elif routerState == 8 :
+        elif routerState == 8:
             return str("Standby")
-        elif routerState == 3 :
+        elif routerState == 3:
             return str("Passive")
-        elif routerState == 8 :
+        elif routerState == 8:
             return str("Standby")
 
-    def GetVirtualIp(self) :
+    def GetVirtualIp(self):
         return str(self.HsrpPacket.virt_ip)
 
-    def GetHelloTime(self) :
+    def GetHelloTime(self):
         return int(self.HsrpPacket.hellotime)
 
-    def GetHoldTime(self) :
+    def GetHoldTime(self):
         return int(self.HsrpPacket.holdtime)
 
-    def GetOpCode(self) :
+    def GetOpCode(self):
         return int(self.HsrpPacket.opcode)
 
-    def GetPriority(self) :
+    def GetPriority(self):
         return int(self.HsrpPacket.priority)
 
-    def GetAuthData(self) :
+    def GetAuthData(self):
         return str(self.HsrpPacket.auth_data)
 
     def GetVersion(self):
         return int(self.HsrpPacket.version)
 
-    def GetGroup(self) :
+    def GetGroup(self):
         return str(self.HsrpPacket.auth_data)
 
     def GetReserved(self):
         return int(self.HsrpPacket.version)
 
     @property
+    def ToJson(self) :
+        if int(self.HsrpPacket.opcode) == 0:
+            PacketToJson = {
+                "HighestLayer": str(self.GetHighestLayerName()),
+                "SourceRouter": str(self.GetSourceRouter()),
+                "DestinationRouter": str(self.GetDestRouter()),
+                "RouterMode": str(self.GetRouterMode()),
+                "RouterState": str(self.GetRouterState()),
+                "AuthData": str(self.GetAuthData()),
+                "HelloTime": str(self.GetHelloTime()),
+                "HoldTime": str(self.GetHoldTime()),
+                "OpCode": str(self.GetOpCode()),
+                "VirtualIP": str(self.GetVirtualIp())
+            }
+        else :
+            PacketToJson = {
+                "HighestLayer" : str(self.GetHighestLayerName()),
+                "SourceRouter": str(self.GetSourceRouter()),
+                "DestinationRouter": str(self.GetDestRouter()),
+                "RouterMode": str(self.GetRouterMode()),
+                "OpCode": str(self.GetOpCode()),
+            }
+        return json.dumps(PacketToJson)
+
+    @property
     def ToString(self) :
         if int(self.HsrpPacket.opcode) == 0 :
-            result = ("Source Router: " + str(self.GetSourceRouter()) + "\n" +
-                      "Destination Router: " + str(self.GetSourceRouter()) + "\n" +
+            result = ("Highest Layer: " + str(self.GetHighestLayerName()) + "\n" +
+                      "Source Router: " + str(self.GetSourceRouter()) + "\n" +
+                      "Destination Router: " + str(self.GetDestRouter()) + "\n" +
                       "Router Mode: " + str(self.GetRouterMode()) + "\n" +
                       "Router State: " + str(self.GetRouterState()) + "\n" +
                       "Auth_Data: " + str(self.GetAuthData()) + "\n" +
@@ -67,8 +99,9 @@ class HSRP :
                       "OpCode: " + str(self.GetOpCode()) + "\n" +
                       "Virtual IP: " + str(self.GetVirtualIp()) + "\n")
         else :
-            result = ("Source Router: " + str(self.GetSourceRouter()) + "\n" +
-                      "Destination Router: " + str(self.GetSourceRouter()) + "\n" +
+            result = ("Highest Layer: " + str(self.GetHighestLayerName()) + "\n" +
+                      "Source Router: " + str(self.GetSourceRouter()) + "\n" +
+                      "Destination Router: " + str(self.GetDestRouter()) + "\n" +
                       "Router Mode: " + str(self.GetRouterMode()) + "\n" +
                       "OpCode: " + str(self.GetOpCode()) + "\n")
         return str(result)
